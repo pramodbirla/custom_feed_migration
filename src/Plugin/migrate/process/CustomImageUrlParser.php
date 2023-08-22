fd96ee6dba6e8844423e23836ec9d52554cbfc27
Pramod Birla <94823772+pramodbirla@users.noreply.github.com>
2023-08-22 14:17:08 +0530

    Update CustomImageUrlParser.php

--- src/Plugin/migrate/process/CustomImageUrlParser.php
+++ src/Plugin/migrate/process/CustomImageUrlParser.php
@@ -18,15 +18,11 @@ class CustomImageUrlParser extends ProcessPluginBase {
   /**
    * {@inheritdoc}
    */
-  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    //\Drupal::logger('custom_feed_migration')->notice(print_r($row, TRUE));
-   
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {  
     $attributes = $value->attributes();
     if (isset($attributes['url'])) {
       return (string) $attributes['url'];
     }
     return NULL;
-    // \Drupal::logger('custom_feed_migration')->notice('SOURCE RESULT: <pre><code>' . print_r($row, TRUE) . '</code></pre>' );
-
   }
 }
